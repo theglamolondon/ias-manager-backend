6d56c819c6a5ec417892b80d5ff2e35de0398fe9
aurelnguetta <nguetta.aurel@fer-ci.org>
2026-08-17 19:57:46 +0000

    sort employe

--- src/main/java/net/ivoireautoservice/ias_manager/controller/EmployeController.java
+++ src/main/java/net/ivoireautoservice/ias_manager/controller/EmployeController.java
@@ -29,7 +29,7 @@ public class EmployeController {
             @RequestParam(required = false) String keyword,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int taille,
-            @RequestParam(defaultValue = "id") String tri,
+            @RequestParam(defaultValue = "nom") String tri,
             @RequestParam(defaultValue = "asc") String ordre) {
         Sort sort = ordre.equalsIgnoreCase("desc") ? Sort.by(tri).descending() : Sort.by(tri).ascending();
         Pageable pageable = PageRequest.of(page, taille, sort);
